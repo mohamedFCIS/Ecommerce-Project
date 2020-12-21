26fc7fd0124124380396314de043c2767fa8f402
mohamedSalah1994 <elkomymohamed69@gmail.com>
2020-12-22 01:17:04 +0200

    edit

--- resources/views/frontEnd/layouts/navbar.blade.php
+++ resources/views/frontEnd/layouts/navbar.blade.php
@@ -1,70 +1,32 @@
 <nav class="colorlib-nav" role="navigation">
     <div class="top-menu">
         <div class="container">
-            <div class="row " >
+            <div class="row">
                 <div class="col-sm-7 col-md-9">
-                    @foreach ($sites as $site)
-                        <div id="colorlib-logo"><a href="{{ url('/home') }}"><img
-                                    src="{{ asset('storage/' . $site->logo_image) }}" alt="{{ $site->logo_name }}"
-                                    class=" object-cover" width="150" height="80">
-                                @if ($site->logo_image != '')
-                                    @break
-                                @endif
-                    @endforeach
-                    </a>
+                    <div id="colorlib-logo"><a href="index.html">Footwear</a></div>
                 </div>
-
+                <div class="col-sm-5 col-md-3">
+                    <form action="#" class="search-wrap">
+                        <div class="form-group">
+                            <input type="search" class="form-control search" placeholder="Search">
+                            <button class="btn btn-primary submit-search text-center" type="submit"><i
+                                    class="icon-search"></i></button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="col-sm-5 col-md-3 ">
-                <form action="{{ route('search') }}" class="search-wrap">
-                    <div class="form-group">
-                        <input type="search" class="form-control search " placeholder="Search" name="search">
-                        <button class="btn btn-primary submit-search text-center" type="submit"><i
-                                class="icon-search"></i></button>
-                    </div>
-                </form>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12 text-left menu-1">
-                <ul>
-                    <li><a href="/home">Home</a></li>
-                    <li class="has-dropdown">
+            <div class="row">
+                <div class="col-sm-12 text-left menu-1">
+                    <ul>
+                        <li ><a href="/home">Home</a></li>
+                        <li class="has-dropdown">
 
-                        {{-- <a href="{{ route('products') }}">Products</a>
-                        --}}
-                        <ul class="dropdown">
-                            <li><a href="product-detail.html">Product Detail</a></li>
-                            @auth
-                                <li> <a href="{{ route('user.fav') }}">User Favourits</a></li>
-                            @endauth
-
-                            <li><a href="cart.html">Shopping Cart</a></li>
-                            <li><a href="{{ route('checkout.index') }}">Checkout</a></li>
-                            <li><a href="order-complete.html">Order Complete</a></li>
-                            <li><a href="add-to-wishlist.html">Wishlist</a></li>
-                        </ul>
-                    </li>
-
-
-
-
-
-                    <li><a href="/aboutUs">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                    <li class="cart">
-                        @if (Route::has('login'))
-                            @if (Auth::check())
-                                @if (Auth::user()->role != 'user')
-                                    <a href="{{ url('/admin/home') }}" class="text-sm text-gray-700 underline "
-                                        style="display: inline">Dashboard</a>
-                                @endif
-                            @endif
-                            <div class="hidden  px-6 py-4 sm:block"
-                                style="padding-top: 0.1rem !important; display:inline">
+                            <a href="">Products</a>
+                            <ul class="dropdown">
+                                <li><a href="product-detail.html">Product Detail</a></li>
                                 @auth
-
-
+                                    <li> <a href="{{route('user.fav')}}">User Favourits</a></li>
+                                @endauth
 
                                 <li><a href="cart.html">Shopping Cart</a></li>
                                 <li><a href="{{route('checkout.index')}}">Checkout</a></li>
@@ -72,104 +34,75 @@
                                 <li><a href="add-to-wishlist.html">Wishlist</a></li>
                             </ul>
                         </li>
-                      
 
-
-                                    <div class="user-area dropdown float-right">
-                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                                            aria-expanded="false">
-                                            @if (Auth::user()->profile_photo_path == '')
-                                                <p>{{ Auth::user()->name }}</p>
-                                            @else
-                                                <img class="rounded-circle float-md-right" width="50" height="50"
-                                                    src="{{ asset('storage/' . Auth::user()->profile_photo_path) }}"
-                                                    alt="{{ Auth::user()->name }}" />
-
-
-                                            @endif
 
 
 
 
                         <li><a href="/aboutUs">About</a></li>
                         <li><a href="/contact">Contact</a></li>
-                        <li class="has-dropdown">
-
-                            <a href="{{route('products')}}">Filters</a>
-                            <ul class="dropdown">
-                                <li><a href="{{route('products.sorted')}}">From Low To High</a></li>
-                                @auth
-                                <li> <a href="{{route('products.desc')}}">From High To Low</a></li>
-                                @endauth
-
-                                <li class="bg-gray-200 rounded-md">
-                                    <form action="{{route('products.filter')}}" method="GET">
-                                        <input class="w-20 rounded-md bg-white p-2 m-2 " type="number" name="max" id="" placeholder="Max">
-                                        <input class="w-20 rounded-md bg-white p-2 m-2" type="number" name="min" id="" placeholder="Min"><br>
-                                        <button class="btn bg-blue-300 text-white m-2" type="submit">Filter</button>
-                                    </form>
-                                </li>
-                                <li><a href="{{route('checkout.index')}}">Checkout</a></li>
-                                <li><a href="order-complete.html">Order Complete</a></li>
-                                <li><a href="add-to-wishlist.html">Wishlist</a></li>
-                            </ul>
-                        </li>
                         <li class="cart">
                             @if (Route::has('login'))
                                 @if (Auth::check())
                                     @if (Auth::user()->role != 'user')
                                         <a href="{{ url('/admin/home') }}"
-                                            class="text-sm text-gray-700 underline " style="display: inline">Dashboard</a>
+                                           class="text-sm text-gray-700 underline " style="display: inline">Dashboard</a>
                                     @endif
                                 @endif
                                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block"
-                                    style="padding-top: 0.1rem !important; display:inline" >
+                                     style="padding-top: 0.1rem !important; display:inline" >
                                     @auth
 
 
-
-                                        </a>
-
-                                        <div class="user-menu dropdown-menu">
-
-
+                                        <div class="user-area dropdown float-right">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                                               aria-expanded="false">
+                                                <img class="rounded-circle float-md-right" width="50" height="50"  src="{{ asset('storage/'. Auth::user()->profile_photo_path )}}" alt="{{ Auth::user()->name }}" />
 
 
-                                            <a class="nav-link" href="{{ route('profile.show') }}"><i class="fa fa-cog"></i>
-                                                Profile</a>
-                                            <form method="POST" action="{{ route('logout') }}">
-                                                @csrf
-                                                <a class="nav-link" href="#" onclick="this.parentNode.submit()"><i
-                                                        class="fa fa-power-off"></i> Logout</a>
 
-                                            </form>
+                                            </a>
 
+                                            <div class="user-menu dropdown-menu">
+
+
+                                                <a class="nav-link" href="#"><i class="fa fa-user"></i> Notifications <span
+                                                        class="count">13</span></a>
+
+                                                <a class="nav-link" href="#"><i class="fa fa-cog"></i> Settings</a>
+                                                <form method="POST" action="{{ route('logout') }}">
+                                                    @csrf
+                                                    <a class="nav-link" href="#" onclick="this.parentNode.submit()"><i
+                                                            class="fa fa-power-off"></i> Logout</a>
+
+                                                </form>
+
+                                            </div>
                                         </div>
-                                    </div>
-                                @else
-                                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+                                    @else
+                                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
 
-                                    @if (Route::has('register'))
-                                        <a href="{{ route('register') }}"
-                                            class="ml-4 text-sm text-gray-700 underline">Register</a>
-                                    @endif
-                                @endauth
-                            </div>
-                        @endif
-
-                    </li>
-                    <li class="cart"><a href="{{ route('cart') }}"><i class="icon-shopping-cart"></i> Cart
-                            @if (Cart::instance('default')->count() > 0)
-                                <strong>[{{ Cart::instance('default')->count() }}]</strong>
+                                        @if (Route::has('register'))
+                                            <a href="{{ route('register') }}"
+                                               class="ml-4 text-sm text-gray-700 underline">Register</a>
+                                        @endif
+                                    @endauth
+                                </div>
                             @endif
-                        </a></li>
+
+                        </li>
+                        <li class="cart"><a href="{{route('cart')}}"><i class="icon-shopping-cart"></i> Cart
+                                @if(Cart::instance('default')->count()>0)
+                                    <strong>[{{Cart::instance('default')->count()}}]</strong>
+                                @endif
+                            </a></li>
 
 
 
-                </ul>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
     </div>
     <div class="sale">
         <div class="container">
